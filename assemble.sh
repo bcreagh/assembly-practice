c7a81a15c63b9793d95a58589f5a5549977f43ae
Brian <briancreagh95@gmail.com>
2018-07-02 22:14:44 +0100

    Put output files into ./output directory

--- assemble.sh
+++ assemble.sh
@@ -8,6 +8,7 @@
 #    ./assemble.sh source
 
 #This will produce an object file called "source.o" and an executable file called "source" 
+#These file will place in ./output
 
-nasm -f elf64 -o $1'.o' $1'.asm'
-ld $1'.o' -o $1
+nasm -f elf64 -o './output/'$1'.o' $1'.asm'
+ld './output/'$1'.o' -o './output/'$1
