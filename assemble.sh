f87244d10c48e9b1796c14874cf50089d04e4037
Brian <briancreagh95@gmail.com>
2018-07-02 21:55:29 +0100

    Add comments for assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#this is a simple script for assembling and linking nasm assembly code
+#it is intended mainly for revision purposes, rather than practical purposes
+
+#usage:
+#    Assuming you have a file called "source.asm",
+#    ./assemble.sh source
+
+#This will produce an object file called "source.o" and an executable file called "source" 
+
 nasm -f elf64 -o $1'.o' $1'.asm'
 ld $1'.o' -o $1
